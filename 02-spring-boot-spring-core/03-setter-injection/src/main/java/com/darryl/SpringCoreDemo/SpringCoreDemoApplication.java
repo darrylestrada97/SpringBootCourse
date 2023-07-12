85fe930e7155d95be4116f25a173e29d6f9d200d
Darryl <darrylestrada97@gmail.com>
2023-07-12 08:57:02 +0200

    updating

---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/darryl/SpringCoreDemo/SpringCoreDemoApplication.java
@@ -0,0 +1,17 @@
+package com.darryl.SpringCoreDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication(
+//		scanBasePackages = {"com.darryl.util","com.darryl.SpringCoreDemo"}
+//)
+@SpringBootApplication
+public class SpringCoreDemoApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreDemoApplication.class, args);
+	}
+
+}
